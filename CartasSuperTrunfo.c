ca3d0082199759fd01e185b239cdf9a4426f5841
a76an <49048511+a76an@users.noreply.github.com>
2026-06-15 16:11:44 -0300

    Add input handling for two city cards

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,47 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    char estado, codigoCarta[3], cidade[25], estado2, codigoCarta2[3], cidade2[25];
+    int populacao, pontosTuristicos, populacao2, pontosTuristicos2;
+    float area, pib, area2, pib2;
+
   // Área para entrada de dados
+    printf("Para a Carta 1:\n");
+    printf("Informe o estado (uma letra de 'A' a 'H'):\n");
+    scanf(" %c", &estado);
+    printf("Informe o código (a letra do estado seguida de um número de 01 a 04, ex: A01, B03):\n");
+    scanf("%s", codigoCarta);
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", cidade);
+    printf("Informe a população:\n");
+    scanf("%d", &populacao);
+    printf("Informe a área (em km²):\n");
+    scanf("%f", &area);
+    printf("Informe o PIB:\n");
+    scanf("%f", &pib);
+    printf("Informe o número de pontos turísticos:\n");
+    scanf("%d", &pontosTuristicos);
+
+    printf("Para a Carta 2:\n");
+    printf("Informe o estado (uma letra de 'A' a 'H'):\n");
+    scanf(" %c", &estado2);
+    printf("Informe o código (a letra do estado seguida de um número de 01 a 04, ex: A01, B03):\n");
+    scanf("%s", codigoCarta2);
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", cidade2);
+    printf("Informe a população:\n");
+    scanf("%d", &populacao2);
+    printf("Informe a área (em km²):\n");
+    scanf("%f", &area2);
+    printf("Informe o PIB:\n");
+    scanf("%f", &pib2);
+    printf("Informe o número de pontos turísticos:\n");
+    scanf("%d", &pontosTuristicos2);
 
   // Área para exibição dos dados da cidade
+    printf("Carta 1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado, codigoCarta, cidade, populacao, area, pontosTuristicos);
+    printf("\n*********************************************\n");
+    printf("Carta 2:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d", estado2, codigoCarta2, cidade2, populacao2, area2, pontosTuristicos2);
 
 return 0;
 } 
